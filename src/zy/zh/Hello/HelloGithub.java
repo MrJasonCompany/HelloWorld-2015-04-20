23506243a02e210583ba10f0d9d4a0911dac7afe
MrDreaming <729637403@qq.com>
2015-04-20 21:13:46 +0800

    使用私人电脑提交代码
    使用私人电脑提交代码

--- src/zy/zh/Hello/HelloGithub.java
+++ src/zy/zh/Hello/HelloGithub.java
@@ -5,6 +5,7 @@ public class HelloGithub {
 		System.out.println("hello github world");
 		System.out.println("2015-4-20 14:45:14");
 		System.out.println("2015-4-20 14:50:10 By MrJasonCompany");
+		System.out.println("使用私人电脑提交@2015-4-20 21:12:50");
 	}
 
 }
