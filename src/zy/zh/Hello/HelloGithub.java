e6aa0d9503651dcc7fd4d347c5c6ded6694a62e0
MrDreaming <729637403@qq.com>
2015-04-20 14:46:52 +0800

    增加日期打印
    增加日期打印

--- src/zy/zh/Hello/HelloGithub.java
+++ src/zy/zh/Hello/HelloGithub.java
@@ -3,6 +3,7 @@ package zy.zh.Hello;
 public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("hello github world");
+		System.out.println("2015-4-20 14:45:14");
 	}
 
 }
