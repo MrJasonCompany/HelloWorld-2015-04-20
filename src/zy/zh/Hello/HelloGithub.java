f451118844cc412bae097fc08b08796424bdfc2f
MrJasonCompany <zhiying_zheng@sina.com>
2015-04-20 14:51:05 +0800

    MrJasonCompany提交
    这次提交作者是MrJasonCompany了吧

--- src/zy/zh/Hello/HelloGithub.java
+++ src/zy/zh/Hello/HelloGithub.java
@@ -4,6 +4,7 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("hello github world");
 		System.out.println("2015-4-20 14:45:14");
+		System.out.println("2015-4-20 14:50:10 By MrJasonCompany");
 	}
 
 }
