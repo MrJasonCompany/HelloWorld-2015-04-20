4e8292abe540129a58c1c2978ce3c4f6a84f9f9b
MrJasonCompany <zhiying_zheng@sina.com>
2015-04-21 16:23:40 +0800

    下午在公司
    下午在公司

--- src/zy/zh/Hello/HelloGithub.java
+++ src/zy/zh/Hello/HelloGithub.java
@@ -5,6 +5,7 @@ public class HelloGithub {
 		System.out.println("hello github world");
 		System.out.println("2015-4-20 14:45:14");
 		System.out.println("2015-4-20 14:50:10 By MrJasonCompany");
+		System.out.println("2015-4-21 16:23:20 下午在公司");
 	}
 
 }
